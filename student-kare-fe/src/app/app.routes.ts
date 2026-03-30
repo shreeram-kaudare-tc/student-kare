62be0ae99a89068b9aea38b843d2dc4055afb6f7
shreekaudare <shreeram.kaudare@thecontrast.in>
2026-03-31 03:23:30 +0530

    register

--- student-kare-fe/src/app/app.routes.ts
+++ student-kare-fe/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginOne } from './pages/login-one/login-one';
 import { LoginFour } from './pages/login-four/login-four';
 import { LoginThree } from './pages/login-three/login-three';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login3', pathMatch: 'full' },
     { path: 'login', component: LoginOne },
     { path: 'login3', component: LoginThree },
     { path: 'login4', component: LoginFour },
+    { path: 'register', component: Register },
 ];
---
+++ student-kare-fe/src/app/pages/register/register.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { InputField } from '../../components/input-field/input-field';
+import { Button } from '../../components/button/button';
+import { AuthLayout1 } from '../../components/auth-layout1/auth-layout1';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, FormsModule,AuthLayout1, InputField, Button],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  firstName = '';
+  lastName = '';
+  email = '';
+  birthDate = '';
+  phoneNumber = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/login3']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login3']);
+  }
+
+  register() {
+    // TODO: integrate register API
+    this.router.navigate(['/home']);
+  }
+}
