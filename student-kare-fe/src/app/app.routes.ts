b9adacb1afd074009d30d582376742161d5c8ce3
shreekaudare <shreeram.kaudare@thecontrast.in>
2026-03-31 03:50:37 +0530

    onboarding

--- student-kare-fe/src/app/app.routes.ts
+++ student-kare-fe/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginOne } from './pages/login-one/login-one';
 import { LoginFour } from './pages/login-four/login-four';
 import { LoginThree } from './pages/login-three/login-three';
 import { Register } from './pages/register/register';
+import { Onboarding } from './pages/onboarding/onboarding';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login3', pathMatch: 'full' },
+    { path: '', redirectTo: 'onboarding', pathMatch: 'full' },
+    { path: 'onboarding', component: Onboarding },
     { path: 'login', component: LoginOne },
     { path: 'login3', component: LoginThree },
     { path: 'login4', component: LoginFour },
---
+++ student-kare-fe/src/app/pages/onboarding/onboarding.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+interface Slide {
+  image: string;
+  title: string;
+  description: string;
+}
+
+@Component({
+  selector: 'app-onboarding',
+  imports: [CommonModule],
+  templateUrl: './onboarding.html',
+  styleUrl: './onboarding.css',
+})
+export class Onboarding {
+  currentIndex = 0;
+
+  slides: Slide[] = [
+    {
+      image: 'images/onboarding/slide-1.png',
+      title: 'Choose your order',
+      description:
+        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+    },
+    {
+      image: 'images/onboarding/slide-2.png',
+      title: 'Choose your product',
+      description:
+        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+    },
+    {
+      image: 'images/onboarding/slide-3.png',
+      title: 'Fast Delivery',
+      description:
+        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+    },
+    {
+      image: 'images/onboarding/slide-4.png',
+      title: 'Promotions',
+      description:
+        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+    },
+  ];
+
+  constructor(private router: Router) { }
+
+  next() {
+    if (this.currentIndex < this.slides.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
+}
