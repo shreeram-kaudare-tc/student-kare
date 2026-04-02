17ecbb18ed8c663ea8006cf9a03e4fe5e39b5d63
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-02 12:34:37 +0530

    asterisk add in otp and password field

--- student-kare-fe/src/app/components/input-field/input-field.ts
+++ student-kare-fe/src/app/components/input-field/input-field.ts
@@ -18,26 +18,67 @@ export class InputField implements ControlValueAccessor {
   @Input() icon: string = '';
   @Input() error: string = '';
   @Input() disabled: boolean = false;
+  @Input() isOtp: boolean = false;
 
   value: string = '';
+  displayValue: string = '';
   showPassword: boolean = false;
 
   private onChange = (_: any) => {};
   private onTouched = () => {};
 
   get inputType(): string {
+    if (this.isOtp) return 'text';
     if (this.type === 'password') return this.showPassword ? 'text' : 'password';
     return this.type;
   }
 
   onInput(event: Event) {
-    this.value = (event.target as HTMLInputElement).value;
+    const el = event.target as HTMLInputElement;
+    const rawValue = el.value;
+
+    if (this.isOtp && !this.showPassword) {
+      // Manual masking for OTP to show asterisks (*)
+      // We check if something was added or deleted
+      if (rawValue.length > this.value.length) {
+        // Appending character
+        const addedChar = rawValue.slice(-1);
+        this.value += addedChar;
+      } else if (rawValue.length < this.value.length) {
+        // Deleting characters
+        this.value = this.value.substring(0, rawValue.length);
+      }
+      
+      this.displayValue = '*'.repeat(this.value.length);
+      el.value = this.displayValue;
+    } else {
+      this.value = rawValue;
+      this.displayValue = rawValue;
+    }
+
     this.onChange(this.value);
   }
 
-  onBlur() { this.onTouched(); }
-  togglePassword() { this.showPassword = !this.showPassword; }
-  writeValue(val: string): void { this.value = val || ''; }
+  onBlur() { 
+    this.onTouched(); 
+  }
+
+  togglePassword() { 
+    this.showPassword = !this.showPassword; 
+    if (this.isOtp) {
+       this.displayValue = this.showPassword ? this.value : '*'.repeat(this.value.length);
+    }
+  }
+
+  writeValue(val: string): void { 
+    this.value = val || ''; 
+    if (this.isOtp && !this.showPassword) {
+      this.displayValue = '*'.repeat(this.value.length);
+    } else {
+      this.displayValue = this.value;
+    }
+  }
+
   registerOnChange(fn: any): void { this.onChange = fn; }
   registerOnTouched(fn: any): void { this.onTouched = fn; }
   setDisabledState(isDisabled: boolean): void { this.disabled = isDisabled; }
