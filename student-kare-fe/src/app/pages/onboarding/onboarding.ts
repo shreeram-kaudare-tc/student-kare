cacabc9989a6c5f181287da9e11dc5f4614687c8
shreekaudare <shreeram.kaudare@thecontrast.in>
2026-04-02 17:17:47 +0530

    chengs

--- student-kare-fe/src/app/pages/onboarding/onboarding.ts
+++ student-kare-fe/src/app/pages/onboarding/onboarding.ts
@@ -8,7 +8,7 @@ interface Slide {
   title: string;
   description: string;
   imgStyle: { width: string; height: string; top: string; left: string };
-  bg_svg_Style: { width: string; height: string; top: string; background: string };
+  bg_svg_Style: { width: string; height: string; bottom: string; background: string };
 }
 
 @Component({
@@ -27,7 +27,7 @@ export class Onboarding {
       title: 'Choose your order',
       description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
       imgStyle: { width: '105.38vw', height: '105.38vw', top: '111px', left: '-4.10vw' },
-      bg_svg_Style: { width: '423.33vw', height: '123.33vw', top: '128px', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
+      bg_svg_Style: { width: '423.33vw', height: '123.33vw', bottom: '0', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
     },
     {
       image: 'images/onboarding/slide-2.png',
@@ -35,7 +35,7 @@ export class Onboarding {
       title: 'Choose your product',
       description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
       imgStyle: { width: '100vw', height: '100vw', top: '127px', left: '0.77vw' },
-      bg_svg_Style: { width: '423.33vw', height: '123.33vw', top: '128px', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
+      bg_svg_Style: { width: '423.33vw', height: '123.33vw', bottom: '30px', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
     },
     {
       image: 'images/onboarding/slide-3.png',
@@ -43,7 +43,7 @@ export class Onboarding {
       title: 'Fast Delivery',
       description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
       imgStyle: { width: '119.74vw', height: '86.15vw', top: '172px', left: '-7.95vw' },
-      bg_svg_Style: { width: '423.33vw', height: '123.33vw', top: '128px', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
+      bg_svg_Style: { width: '423.33vw', height: '123.33vw', bottom: '30px', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
     },
     {
       image: 'images/onboarding/slide-4.png',
@@ -51,7 +51,7 @@ export class Onboarding {
       title: 'Promotions',
       description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
       imgStyle: { width: '88.97vw', height: '88.97vw', top: '183px', left: '2.82vw' },
-      bg_svg_Style: { width: '430.00vw', height: '121.79vw', top: '136px', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
+      bg_svg_Style: { width: '430.00vw', height: '121.79vw', bottom: '90px', background: 'linear-gradient(0deg, #3E63DD, #3E63DD), linear-gradient(90deg, #6B61FD 1%, #4E75F4 32.5%, #8060FF 90%)' },
     },
   ];
 
