9b43d601c374e7f8758d7d2d3032a4b727b6e0c6
shreekaudare <shreeram.kaudare@thecontrast.in>
2026-04-02 15:32:17 +0530

    postion adjest

--- student-kare-fe/src/app/pages/onboarding/onboarding.ts
+++ student-kare-fe/src/app/pages/onboarding/onboarding.ts
@@ -7,6 +7,8 @@ interface Slide {
   bgSvg: string;
   title: string;
   description: string;
+  imgStyle: { width: string; height: string; top: string; left: string };
+  bg_svg_Style: { width: string; height: string; top: string; opacity: string };
 }
 
 @Component({
@@ -23,29 +25,34 @@ export class Onboarding {
       image: 'images/onboarding/slide-1.png',
       bgSvg: 'images/svg/onboarding-one.svg',
       title: 'Choose your order',
-      description:
-        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      imgStyle: { width: '105.38vw', height: '105.38vw', top: '111px', left: '-4.10vw' },
+      bg_svg_Style: { width: '423.33vw', height: '123.33vw', top: '128px', opacity: '0.19' },
     },
     {
       image: 'images/onboarding/slide-2.png',
       bgSvg: 'images/svg/onboarding-two.svg',
       title: 'Choose your product',
-      description:
-        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      imgStyle: { width: '100vw', height: '100vw', top: '127px', left: '0.77vw' },
+      bg_svg_Style: { width: '423.33vw', height: '123.33vw', top: '128px', opacity: '0.19' },
     },
     {
       image: 'images/onboarding/slide-3.png',
       bgSvg: 'images/svg/onboarding-three.svg',
       title: 'Fast Delivery',
-      description:
-        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      imgStyle: { width: '119.74vw', height: '86.15vw', top: '172px', left: '-7.95vw' },
+      bg_svg_Style: { width: '423.33vw', height: '123.33vw', top: '128px', opacity: '0.19' },
     },
     {
       image: 'images/onboarding/slide-4.png',
       bgSvg: 'images/svg/onboarding-four.svg',
+      
       title: 'Promotions',
-      description:
-        'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      description: 'Regular Fit Half Sleeve Polo T-Shirt made of comfortable Cotton Polyester Pique fabric with Embroidered logo at left',
+      imgStyle: { width: '88.97vw', height: '88.97vw', top: '183px', left: '2.82vw' },
+      bg_svg_Style: { width: '430.00vw', height: '121.79vw', top: '136px', opacity: '0.19' },
     },
   ];
 
